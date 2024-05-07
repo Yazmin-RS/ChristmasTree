87fc2f1503d7e9547397a03f1f82f85e067f8ea4
Yazmin Rodriguez <yazmin.rs.contacto@gmail.com>
2024-05-07 11:48:42 -0600

    último cambio

--- CristmasProject/src/Main.java
+++ CristmasProject/src/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
         int height = 10; // Altura del árbol
 
-        for (int i = 0; i < height; i++) {
-            System.out.print(" "); // Espacios para mover la estrella hacia la derecha
-        }
+        System.out.printf("%" + height + "s", ""); // Espacio para poner la estrella en el centro
         System.out.println("*");
+
         for (int i = 0; i < height - 1; i++) {
             System.out.printf("%" + (height - i) + "s", ""); // Espacio para poner los ceros a la derecha
             System.out.println("0".repeat(2 * i + 1)); // Imprime los ceros del árbol
